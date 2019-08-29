61a1987e9a03210c0b9956505effa123f6646b75
EDave64 <nord3rn@gmail.com>
2019-08-29 19:11:23 +0200

    Changed MC chibis and credit

--- src/components/panels/credits.vue
+++ src/components/panels/credits.vue
@@ -2,10 +2,13 @@
 	<div :class="{ panel: true, vertical }">
 		<h1>Credits</h1>
 		<p>All assets used are IP of Team Salvato and created by them, unless noted otherwise.</p>
-		<p>Doki Doki Dialog Generator developed by EDave64</p>
+		<p>
+			Doki Doki Dialog Generator developed by EDave64
+			<br />Based on gemdude46's DDLC Shitpost generator
+		</p>
 		<p>MC Classic Sprite by Childish-N</p>
 		<p>MC Sprite by Childish-N</p>
-		<p>MC Chibis from Seanm0451's Chibi Pack</p>
+		<p>MC Chibis by GyleToTheRescue</p>
 		<p>
 			FeMC Sprite by Meddy-sin
 			<br />FeMC Casual Sprite by SlightlySimple
