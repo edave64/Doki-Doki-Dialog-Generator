489d1fa86aca0fea0116e103d2ed2735d0366fe6
EDave64 <nord3rn@gmail.com>
2019-09-15 22:19:11 +0200

    Added credits for the placeholder background

--- src/components/panels/credits.vue
+++ src/components/panels/credits.vue
@@ -3,6 +3,10 @@
 		<h1>Credits</h1>
 		<p>All assets used are IP of Team Salvato and created by them, unless noted otherwise.</p>
 		<p>
+			Unused House Background by
+			<a href="https://www.deviantart.com/fatelogic/art/VN-Background-Practice-295671161">Fatelogic</a>
+		</p>
+		<p>
 			Doki Doki Dialog Generator developed by EDave64
 			<br />Based on gemdude46's DDLC Shitpost generator
 		</p>
