4992bbb83000c130f42143079159f84e54097b58
EDave64 <nord3rn@gmail.com>
2019-10-31 17:23:19 +0100

    Fixed PanelMixin scrolling in Firefox and Edge

--- src/components/panels/panelMixin.ts
+++ src/components/panels/panelMixin.ts
@@ -4,15 +4,24 @@ import Component from 'vue-class-component';
 @Component({})
 export class PanelMixin extends Vue {
 	private mounted() {
-		this.$el.addEventListener('wheel', e => {
-			if (e.type !== 'wheel') return;
-			if (!e.currentTarget) return;
-			const target = e.currentTarget as HTMLElement;
-			if (target.classList.contains('vertical')) return;
+		this.$el.addEventListener(
+			'wheel',
+			e => {
+				if (e.type !== 'wheel') return;
+				if (!e.currentTarget) return;
+				const target = e.currentTarget as HTMLElement;
+				if (target.classList.contains('vertical')) return;
 
-			const ev = e as WheelEvent;
-			if (ev.deltaY === 0) return;
-			target.scrollBy({ top: 0, left: ev.deltaY, behavior: 'auto' });
-		});
+				const ev = e as WheelEvent;
+				if (ev.deltaY === 0) return;
+				if ((ev as any).mozInputSource) {
+					// Firefox sends wierdly low delta values :/
+					target.scrollLeft += ev.deltaY * 25;
+				} else {
+					target.scrollLeft += ev.deltaY;
+				}
+			},
+			{ passive: true }
+		);
 	}
 }
