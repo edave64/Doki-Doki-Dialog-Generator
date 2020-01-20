386245c0767c54b1c419ece9ccdecc72f57ed6fc
EDave64 <nord3rn@gmail.com>
2020-01-20 23:57:35 +0100

    Grid for the add panel

--- src/components/toolbox/panels/add.vue
+++ src/components/toolbox/panels/add.vue
@@ -1,14 +1,16 @@
 <template>
 	<div class="panel" v-if="isWebPSupported !== undefined">
 		<h1>Add character</h1>
-		<div
-			class="character"
-			v-for="character of characters"
-			:key="character.id"
-			:title="character.name"
-			@click="onChosen(character.id.toLowerCase())"
-		>
-			<img :src="assetPath(character)" :alt="character.name" />
+		<div :class="{characterGrid: true, vertical }">
+			<div
+				class="character"
+				v-for="character of characters"
+				:key="character.id"
+				:title="character.name"
+				@click="onChosen(character.id.toLowerCase())"
+			>
+				<img :src="assetPath(character)" :alt="character.name" />
+			</div>
 		</div>
 		<div class="btn custom-sprite" @click="$refs.upload.click()">
 			Custom sprite upload
@@ -33,6 +35,7 @@ import { Character } from '@edave64/doki-doki-dialog-generator-pack-format/dist/
 import { Store } from 'vuex';
 import { IRootState } from '../../../store';
 import { PanelMixin } from './panelMixin';
+import { State } from 'vuex-class-decorator';
 
 @Component({
 	components: {},
@@ -95,12 +98,6 @@ textarea {
 	&:not(.vertical) .panel {
 		justify-content: center;
 	}
-
-	&.vertical .panel {
-		.character {
-			text-align: center;
-		}
-	}
 }
 
 .panel {
@@ -110,6 +107,36 @@ textarea {
 		input {
 			display: none;
 		}
+	}
+}
+
+.characterGrid {
+	overflow: hidden;
+	display: grid;
+	place-items: center;
+
+	&.vertical {
+		width: 100%;
+		grid-template-columns: 1fr 1fr 1fr;
+		grid-auto-rows: 72px;
+		grid-auto-flow: row;
+	}
+
+	&:not(.vertical) {
+		height: 100%;
+		grid-template-rows: 1fr 1fr;
+		grid-auto-columns: 72px;
+		grid-auto-flow: column;
+	}
+
+	.character {
+		align-self: center;
+		justify-self: center;
+	}
+
+	.character img {
+		max-height: 72px;
+		max-width: 72px;
 	}
 }
 </style>
