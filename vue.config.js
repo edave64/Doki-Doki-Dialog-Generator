4b8fa45da2a21ac380180ee5b8e8392c33dbeb56
EDave64 <nord3rn@gmail.com>
2020-01-30 19:47:25 +0100

    Removed log from config file

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,6 @@ module.exports = {
 	publicPath: '.',
 	chainWebpack: config => {
 		config.plugin('copy').tap(args => {
-			console.log(args);
 			const config = args[0][0];
 			if (process.env.DDDGElectron) {
 				config.ignore.push('**/*.lq.webp');
