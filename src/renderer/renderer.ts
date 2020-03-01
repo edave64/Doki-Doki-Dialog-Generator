20300be1e8246d92c14ee39dfc50bbda49276595
EDave64 <nord3rn@gmail.com>
2020-03-01 11:48:59 +0100

    Use environment specific export

--- src/renderer/renderer.ts
+++ src/renderer/renderer.ts
@@ -1,5 +1,6 @@
 import { RenderContext } from './rendererContext';
 import { RenderAbortedException } from './renderAbortedException';
+import environment from '../environments/environment';
 
 export class Renderer {
 	private previewCanvas: HTMLCanvasElement;
@@ -71,65 +72,12 @@ export class Renderer {
 		ctx.clearRect(0, 0, this.previewCanvas.width, this.previewCanvas.height);
 		await renderCallback(new RenderContext(ctx, true, false));
 
-		let url;
-		if (undefined === window.navigator.msSaveOrOpenBlob) {
-			const a = document.createElement('a');
-			a.setAttribute('download', filename);
-			url = await this.createObjectURL(downloadCanvas);
-			a.setAttribute('href', url);
-			document.body.appendChild(a);
-			a.click();
-			document.body.removeChild(a);
-		} else {
-			url = downloadCanvas.toDataURL();
-			const blob = this.dataURItoBlob(url);
-
-			if (window.URL && window.URL.createObjectURL) {
-				url = URL.createObjectURL(blob);
-			}
-
-			// IE-specific code
-			window.navigator.msSaveBlob(blob, filename);
-		}
-
-		return url;
-	}
-
-	public createObjectURL(canvas: HTMLCanvasElement): Promise<string> {
-		return new Promise((resolve, reject) => {
-			if (canvas.toBlob && window.URL && window.URL.createObjectURL) {
-				canvas.toBlob(blob => {
-					if (!blob) {
-						reject();
-						return;
-					}
-					resolve(URL.createObjectURL(blob));
-				}, 'image/png');
-			} else if (window.URL && window.URL.createObjectURL) {
-				const url = canvas.toDataURL();
-				const blob = this.dataURItoBlob(url);
-				resolve(URL.createObjectURL(blob));
-			} else {
-				resolve(canvas.toDataURL());
-			}
-		});
+		return environment.saveToFile(downloadCanvas, filename);
 	}
 
 	public getDataAt(x: number, y: number): Uint8ClampedArray {
 		const ctx = this.previewCanvas.getContext('2d');
 
 		return ctx!.getImageData(x, y, 1, 1).data;
-	}
-
-	private dataURItoBlob(dataURI: string) {
-		const binStr = atob(dataURI.split(',')[1]);
-		const len = binStr.length;
-		const arr = new Uint8Array(len);
-
-		for (let i = 0; i < len; i++) {
-			arr[i] = binStr.charCodeAt(i);
-		}
-
-		return new Blob([arr], { type: 'image/png' });
 	}
 }
