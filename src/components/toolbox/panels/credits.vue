0c7bf89c81dff764c574d6d22763b6e8cd727066
EDave64 <nord3rn@gmail.com>
2020-03-21 18:47:03 +0100

    Proper crediting for all fonts

--- src/components/toolbox/panels/credits.vue
+++ src/components/toolbox/panels/credits.vue
@@ -6,13 +6,15 @@
 			target="_blank"
 			rel="noopener noreferrer"
 			href="https://github.com/edave64/Doki-Doki-Dialog-Generator/blob/v1/docs/index.md"
-		>Help</a>
+			>Help</a
+		>
 		<a
 			class="btn"
 			target="_blank"
 			rel="noopener noreferrer"
 			href="https://github.com/edave64/Doki-Doki-Dialog-Generator/"
-		>Fork me on GitHub</a>
+			>Fork me on GitHub</a
+		>
 		<p>
 			All assets used are IP of Team Salvato and created by them, unless noted
 			otherwise.
@@ -23,7 +25,108 @@
 				target="_blank"
 				rel="noopener noreferrer"
 				href="https://www.deviantart.com/fatelogic/art/VN-Background-Practice-295671161"
-			>Fatelogic</a>
+				>Fatelogic</a
+			>
+		</p>
+		<p>
+			Aller Font by
+			<a
+				target="_blank"
+				rel="noopener noreferrer"
+				href="https://www.daltonmaag.com/library/aller"
+				>Dalton Maag Ltd</a
+			>
+		</p>
+		<p>
+			Riffic Bold Font by
+			<a
+				target="_blank"
+				rel="noopener noreferrer"
+				href="https://www.fontspring.com/fonts/inky-type/riffic"
+				>InkyType</a
+			>
+		</p>
+		<p>
+			Verily Serif Mono Font by
+			<a
+				target="_blank"
+				rel="noopener noreferrer"
+				href="https://www.dafont.com/verily-serif-mono.font"
+				>Stephen G. Hartke</a
+			>
+		</p>
+		<p>
+			F25 Bank Printer Font by
+			<a
+				target="_blank"
+				rel="noopener noreferrer"
+				href="https://www.dafont.com/f25-bank-printer.font"
+				>F25 Digital Typeface Design</a
+			>
+		</p>
+		<p>
+			Journal Font by
+			<a
+				target="_blank"
+				rel="noopener noreferrer"
+				href="https://www.dafont.com/journal.font"
+				>Fontourist</a
+			>
+		</p>
+		<p>
+			Hashtag Font by
+			<a
+				target="_blank"
+				rel="noopener noreferrer"
+				href="https://www.dafont.com/hashtag.font"
+			>
+				TitanVex</a
+			>
+		</p>
+		<p>
+			JP Hand Slanted Font by
+			<a
+				target="_blank"
+				rel="noopener noreferrer"
+				href="https://www.dafont.com/jp-hand-slanted.font"
+				>Jonathan Paterson</a
+			>
+		</p>
+		<p>
+			As I Lay Dying Font by
+			<a
+				target="_blank"
+				rel="noopener noreferrer"
+				href="https://www.dafont.com/as-i-lay-dying.font"
+				>Moises Esqueda</a
+			>
+		</p>
+		<p>
+			Damagrafik Script Font by
+			<a
+				target="_blank"
+				rel="noopener noreferrer"
+				href="https://www.dafont.com/damagrafik-script.font"
+				>Davide Terenzi</a
+			>
+		</p>
+		<p>
+			Ammy's Handwriting Font by
+			<a
+				target="_blank"
+				rel="noopener noreferrer"
+				href="https://www.dafont.com/ammys-handwriting.font"
+				>Ammy K.</a
+			>
+		</p>
+		<p>
+			Halogen Font by
+			<a
+				target="_blank"
+				rel="noopener noreferrer"
+				href="https://www.dafont.com/halogen.font"
+				>JLH Fonts</a
+			>
 		</p>
 		<p>
 			Doki Doki Dialog Generator developed by EDave64
@@ -35,13 +138,11 @@
 		</p>
 		<p>
 			MC Sprite by Childish-N
-			<br />MC Casual Sprite by SlightlySimple
-			<br />MC Chibi by SlightlySimple
+			<br />MC Casual Sprite by SlightlySimple <br />MC Chibi by SlightlySimple
 		</p>
 		<p>
 			MC Chad Sprite by Cylent-Nite
-			<br />MC Chad Poses and expressions by SlightlySimple
-			<br />MC Chad Poses
+			<br />MC Chad Poses and expressions by SlightlySimple <br />MC Chad Poses
 			and chibi by Meddy-sin
 		</p>
 		<p>
