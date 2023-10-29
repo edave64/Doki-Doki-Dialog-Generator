8882d16d620fe0534a10535116b00447430e761e
edave64 <github5815@xcvbnm.org>
2023-10-29 22:32:35 +0100

    Add UI in setup syntax

--- src/components/toolbox/tools/add/ui.vue
+++ src/components/toolbox/tools/add/ui.vue
@@ -1,41 +1,39 @@
 <template>
 	<button class="v-w100" @click="addTextBox">Textbox</button>
-	<button class="v-w100" @click="addPoem">Poem</button>
-	<button class="v-w100" @click="addDialog">Notification</button>
-	<button class="v-w100" @click="addChoice">Choice</button>
-	<button class="v-w100" @click="addConsole">Console</button>
+	<button class="v-w100 v-bt0" @click="addPoem">Poem</button>
+	<button class="v-w100 v-bt0" @click="addDialog">Notification</button>
+	<button class="v-w100 v-bt0" @click="addChoice">Choice</button>
+	<button class="v-w100 v-bt0" @click="addConsole">Console</button>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import { transaction } from '@/plugins/vuex-history';
-import { defineComponent } from 'vue';
+import { IRootState } from '@/store';
+import { Store, useStore } from 'vuex';
 
-export default defineComponent({
-	methods: {
-		async addTextBox() {
-			return await this.createUiElement('createTextBox');
-		},
-		async addChoice() {
-			return await this.createUiElement('createChoice');
-		},
-		async addDialog() {
-			return await this.createUiElement('createNotification');
-		},
-		async addPoem() {
-			return await this.createUiElement('createPoem');
-		},
-		async addConsole() {
-			return await this.createUiElement('createConsole');
-		},
-		async createUiElement(messageName: string): Promise<void> {
-			await transaction(async () => {
-				await this.$store.dispatch(`panels/${messageName}`, {
-					panelId: this.$store.state.panels.currentPanel,
-				});
-			});
-		},
-	},
-});
+const store = useStore() as Store<IRootState>;
+async function addTextBox() {
+	return await createUiElement('createTextBox');
+}
+async function addChoice() {
+	return await createUiElement('createChoice');
+}
+async function addDialog() {
+	return await createUiElement('createNotification');
+}
+async function addPoem() {
+	return await createUiElement('createPoem');
+}
+async function addConsole() {
+	return await createUiElement('createConsole');
+}
+async function createUiElement(messageName: string): Promise<void> {
+	await transaction(async () => {
+		await store.dispatch(`panels/${messageName}`, {
+			panelId: store.state.panels.currentPanel,
+		});
+	});
+}
 </script>
 
 <style lang="scss" scoped></style>
