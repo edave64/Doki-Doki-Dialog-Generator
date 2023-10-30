97a5d4d544694a53279c81dbfe00cdf1814d766d
edave64 <github5815@xcvbnm.org>
2023-10-30 11:21:43 +0100

    Link setup syntax

--- src/components/ui/link.vue
+++ src/components/ui/link.vue
@@ -1,44 +1,39 @@
 <!--
 	A link to another website. Supports special prefixes "wiki:" to reference the github wiki and "github:" to link the
 	projects github page.
+    a-tag links by default kinda suck for web apps, since they leave the current
+    page and do funny stuff with shared javascript contexts.
+    This takes care of the boilerplate
 -->
 <template>
-	<a target="_blank" rel="noopener noreferrer" :href="href">
+	<a target="_blank" rel="noopener noreferrer" ref="root" :href="href">
 		<slot />
 	</a>
 </template>
 
-<script lang="ts">
-import { VerticalScrollRedirect } from '@/components/mixins/vertical-scroll-redirect';
-import { defineComponent } from 'vue';
-/**
- * a-tag links by default kinda suck for web apps, since they leave the current
- * page and do funny stuff with shared javascript contexts.
- * This takes care of the boilerplate
- */
-export default defineComponent({
-	mixins: [VerticalScrollRedirect],
-	props: {
-		to: {
-			type: String,
-			required: true,
-		},
+<script lang="ts" setup>
+import { computed, ref } from 'vue';
+
+const root = ref(null! as HTMLElement);
+const props = defineProps({
+	to: {
+		type: String,
+		required: true,
 	},
-	computed: {
-		href(): string {
-			let to = this.to;
-			to = to.replace(
-				/^github:\/\//,
-				'https://github.com/edave64/Doki-Doki-Dialog-Generator/'
-			);
-			to = to.replace(/^wiki:\/\/(.*)/, (_, page) => {
-				return (
-					'https://github.com/edave64/Doki-Doki-Dialog-Generator/wiki/Version-2:-' +
-					page.replaceAll(' ', '-')
-				);
-			});
-			return to;
-		},
-	},
+});
+
+const href = computed((): string => {
+	let to = props.to;
+	to = to.replace(
+		/^github:\/\//,
+		'https://github.com/edave64/Doki-Doki-Dialog-Generator/'
+	);
+	to = to.replace(/^wiki:\/\/(.*)/, (_, page) => {
+		return (
+			'https://github.com/edave64/Doki-Doki-Dialog-Generator/wiki/Version-2:-' +
+			page.replaceAll(' ', '-')
+		);
+	});
+	return to;
 });
 </script>
