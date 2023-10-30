7c793216ba7b7746765a07d603344481b9c45afa
edave64 <github5815@xcvbnm.org>
2023-10-30 10:31:47 +0100

    Add character tool to setup syntax

--- src/components/toolbox/tools/add/character.vue
+++ src/components/toolbox/tools/add/character.vue
@@ -14,48 +14,45 @@
 	<d-button
 		class="custom-sprite"
 		icon="extension"
-		@click="$emit('show-dialog', 'type: Characters')"
+		@click="emit('show-dialog', 'type: Characters')"
 	>
 		Search in content packs
 	</d-button>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import DButton from '@/components/ui/d-button.vue';
 import environment from '@/environments/environment';
 import { transaction } from '@/plugins/vuex-history';
+import { useStore } from '@/store';
 import { IAssetSwitch } from '@/store/content';
 import { ICreateCharacterAction } from '@/store/object-types/characters';
 import { Character } from '@edave64/doki-doki-dialog-generator-pack-format/dist/v2/model';
-import { DeepReadonly } from 'ts-essentials';
-import { defineComponent } from 'vue';
+import { computed } from 'vue';
 
-export default defineComponent({
-	emits: ['show-dialog'],
-	components: { DButton },
-	computed: {
-		characters(): DeepReadonly<Array<Character<IAssetSwitch>>> {
-			return this.$store.state.content.current.characters;
-		},
-	},
-	methods: {
-		assetPath(character: Character<IAssetSwitch>) {
-			return character.chibi
-				? environment.supports.lq
-					? character.chibi.lq
-					: character.chibi.hq
-				: '';
-		},
-		onChosen(id: string) {
-			transaction(async () => {
-				await this.$store.dispatch('panels/createCharacters', {
-					characterType: id,
-					panelId: this.$store.state.panels.currentPanel,
-				} as ICreateCharacterAction);
-			});
-		},
-	},
+const store = useStore();
+const emit = defineEmits(['show-dialog']);
+
+const characters = computed(() => {
+	return store.state.content.current.characters;
 });
+
+function assetPath(character: Character<IAssetSwitch>) {
+	return character.chibi
+		? environment.supports.lq
+			? character.chibi.lq
+			: character.chibi.hq
+		: '';
+}
+
+function onChosen(id: string) {
+	transaction(async () => {
+		await store.dispatch('panels/createCharacters', {
+			characterType: id,
+			panelId: store.state.panels.currentPanel,
+		} as ICreateCharacterAction);
+	});
+}
 </script>
 
 <style lang="scss" scoped>
