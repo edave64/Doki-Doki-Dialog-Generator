9dbf8703a658e5666dcbcfc1f4f89fb0a9d72564
edave64 <github5815@xcvbnm.org>
2023-10-31 21:53:43 +0100

    Beckground settings in setup syntax

--- src/components/toolbox/tools/background/settings.vue
+++ src/components/toolbox/tools/background/settings.vue
@@ -9,7 +9,7 @@
 				id="bg_color"
 				class="color-button"
 				:style="{ background: color }"
-				@click="$emit('change-color')"
+				@click="emit('change-color')"
 			/>
 		</template>
 		<table v-if="isVariant">
@@ -41,7 +41,7 @@
 					<button
 						id="image_options_button"
 						class="v-w100"
-						@click="$emit('open-image-options')"
+						@click="emit('open-image-options')"
 					>
 						Image options
 					</button>
@@ -51,13 +51,13 @@
 	</d-fieldset>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import Toggle from '@/components/toggle.vue';
 import DFieldset from '@/components/ui/d-fieldset.vue';
 import { transaction } from '@/plugins/vuex-history';
+import { useStore } from '@/store';
 import { IAssetSwitch } from '@/store/content';
 import {
-	IPanel,
 	ISeekVariantAction,
 	ISetColorMutation,
 	ISetFlipMutation,
@@ -65,89 +65,83 @@ import {
 } from '@/store/panels';
 import { Background } from '@edave64/doki-doki-dialog-generator-pack-format/dist/v2/model';
 import { DeepReadonly } from 'ts-essentials';
-import { defineComponent } from 'vue';
+import { computed } from 'vue';
 
-export default defineComponent({
-	components: { Toggle, DFieldset },
-	computed: {
-		vertical(): boolean {
-			return this.$store.state.ui.vertical;
-		},
-		color: {
-			get(): string {
-				return this.background.color;
-			},
-			set(color: string): void {
-				transaction(() => {
-					this.$store.commit('panels/setBackgroundColor', {
-						color,
-						panelId: this.$store.state.panels.currentPanel,
-					} as ISetColorMutation);
-				});
-			},
-		},
-		flipped: {
-			get(): boolean {
-				return this.background.flipped;
-			},
-			set(flipped: boolean) {
-				transaction(() => {
-					this.$store.commit('panels/setBackgroundFlipped', {
-						flipped,
-						panelId: this.$store.state.panels.currentPanel,
-					} as ISetFlipMutation);
-				});
-			},
-		},
-		scaling: {
-			get(): string {
-				return this.background.scaling.toString();
-			},
-
-			set(scaling: string) {
-				transaction(() => {
-					this.$store.commit('panels/setBackgroundScaling', {
-						scaling: parseInt(scaling, 10),
-						panelId: this.$store.state.panels.currentPanel,
-					} as ISetScalingMutation);
-				});
-			},
-		},
-		currentBackgroundId(): string {
-			return this.background.current;
-		},
-		background(): DeepReadonly<IPanel['background']> {
-			const currentPanel = this.$store.state.panels.currentPanel;
-			return this.$store.state.panels.panels[currentPanel].background;
-		},
-		bgData(): DeepReadonly<Background<IAssetSwitch>> | null {
-			return (
-				this.$store.state.content.current.backgrounds.find(
-					(background) => background.id === this.currentBackgroundId
-				) || null
-			);
-		},
-		isColor(): boolean {
-			return this.currentBackgroundId === 'buildin.static-color';
-		},
-		isVariant(): boolean {
-			return !!this.bgData;
-		},
-		hasVariants(): boolean {
-			return this.bgData ? this.bgData.variants.length > 1 : false;
-		},
+const store = useStore();
+const emit = defineEmits(['open-image-options', 'change-color']);
+const vertical = computed(() => store.state.ui.vertical);
+const background = computed(() => {
+	const currentPanel = store.state.panels.currentPanel;
+	return store.state.panels.panels[currentPanel].background;
+});
+const flipped = computed({
+	get(): boolean {
+		return background.value.flipped;
 	},
-	methods: {
-		seekVariant(delta: 1 | -1) {
-			transaction(async () => {
-				await this.$store.dispatch('panels/seekBackgroundVariant', {
-					delta,
-					panelId: this.$store.state.panels.currentPanel,
-				} as ISeekVariantAction);
-			});
-		},
+	set(flipped: boolean) {
+		transaction(() => {
+			store.commit('panels/setBackgroundFlipped', {
+				flipped,
+				panelId: store.state.panels.currentPanel,
+			} as ISetFlipMutation);
+		});
 	},
 });
+const scaling = computed({
+	get(): string {
+		return background.value.scaling.toString();
+	},
+
+	set(scaling: string) {
+		transaction(() => {
+			store.commit('panels/setBackgroundScaling', {
+				scaling: parseInt(scaling, 10),
+				panelId: store.state.panels.currentPanel,
+			} as ISetScalingMutation);
+		});
+	},
+});
+const currentBackgroundId = computed(() => background.value.current);
+const bgData = computed((): DeepReadonly<Background<IAssetSwitch>> | null => {
+	return (
+		store.state.content.current.backgrounds.find(
+			(background) => background.id === currentBackgroundId.value
+		) || null
+	);
+});
+//#region Static color
+const isColor = computed(
+	() => currentBackgroundId.value === 'buildin.static-color'
+);
+const color = computed({
+	get(): string {
+		return background.value.color;
+	},
+	set(color: string): void {
+		transaction(() => {
+			store.commit('panels/setBackgroundColor', {
+				color,
+				panelId: store.state.panels.currentPanel,
+			} as ISetColorMutation);
+		});
+	},
+});
+//#endregion Static color
+//#region Variants
+const isVariant = computed(() => !!bgData.value);
+const hasVariants = computed(() =>
+	bgData.value ? bgData.value.variants.length > 1 : false
+);
+
+function seekVariant(delta: 1 | -1) {
+	transaction(async () => {
+		await store.dispatch('panels/seekBackgroundVariant', {
+			delta,
+			panelId: store.state.panels.currentPanel,
+		} as ISeekVariantAction);
+	});
+}
+//#endregion Variants
 </script>
 
 <style lang="scss" scoped>
