f7816b3e32498dfde6a4e30a2dc5ec18c4044105
edave64 <github5815@xcvbnm.org>
2023-10-31 18:29:28 +0100

    Selector in setup syntax

--- src/components/content-pack-builder/expression-builder/selector.vue
+++ src/components/content-pack-builder/expression-builder/selector.vue
@@ -4,11 +4,7 @@
 	</div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-
-export default defineComponent({});
-</script>
+<script lang="ts" setup></script>
 
 <style scoped>
 .selector {
