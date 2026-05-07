a220283f4c67c7ea3b4b300d157cd86784b1dc51
edave64 <github5815@xcvbnm.org>
2026-05-07 23:33:09 +0200

    tests(visual-regression-testing): Test improvements

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,10 +17,8 @@ export default defineConfig({
 	fullyParallel: true,
 	/* Fail the build on CI if you accidentally left test.only in the source code. */
 	forbidOnly: !!process.env.CI,
-	/* Retry on CI only */
-	retries: process.env.CI ? 2 : 0,
-	/* Opt out of parallel tests on CI. */
-	workers: process.env.CI ? 1 : undefined,
+	/* Image export shouldn't be flaky. No need to retry. */
+	retries: 0,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
 	// reporter: 'dot',
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
